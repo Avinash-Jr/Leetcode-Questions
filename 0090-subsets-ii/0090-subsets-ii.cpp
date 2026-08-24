4fd20ce89481fa44c287001577b2c50cc0b47c32
Avinash Kumar <91388079+Avinash-Jr@users.noreply.github.com>
2026-08-24 13:31:37 +0530

    Time: 1 ms (44.88%), Space: 10.4 MB (67.76%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
-        void solve(vector<int>& nums,int index,vector<int>& current,
-        vector<vector<int>>& ans
-    )
-    {
-        // BASE CASE
-        if (index == nums.size()){
-            ans.push_back(current);
-            return;
-        }
-        // CASE 1: INCLUDE nums[index]
-        current.push_back(nums[index]);
-        solve(nums,index + 1,current,ans);
-
-        // BACKTRACK
-        current.pop_back();
-        // CASE 2: EXCLUDE nums[index]
-        /*
-            If the next elements are duplicates,
-            skip them.
-                nums = [1,2,2]
-                index = 1
-                We exclude the first 2.
-                Move directly to the end of all
-                consecutive 2s.
-        */
-        int next = index + 1;
-        while (next < nums.size() && nums[next] == nums[index])
-        {
-            next++;
-        }
-        solve(nums,next,current,ans);
-    }
-
-    vector<vector<int>> subsetsWithDup(vector<int>& nums)
-    {
-        vector<vector<int>> ans;
-        vector<int> current;
-        // Put duplicates together.
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        solve(nums,0,current,ans);
-        return ans;
+        vector<vector<int>> res = {{}};
+        int prevIdx = 0;
+        int idx = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            idx = (i >= 1 && nums[i] == nums[i - 1]) ? prevIdx : 0;
+            prevIdx = res.size();
+            for (int j = idx; j < prevIdx; j++) {
+                std:vector<int> tmp = res[j];
+                tmp.push_back(nums[i]);
+                res.push_back(tmp);
+            }
+        }
+
+        return res;
     }
 };
-
